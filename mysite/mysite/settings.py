d902158a385ff1e7fd8638bc1322c8a2fe807ea9
Yongyan Liu <legolyy@tamu.edu>
2018-04-30 02:20:03 -0500

    finish doc tutorial part 7

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # DIRS is a list of filesystem directories to check when loading Django templates; it’s a search path.
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    # '/var/www/static/',
+]
